ba20977cca780bc0848e3d1b4c4418b160fca322
Seyed Mohammad <moini87@yahoo.com>
2021-07-22 12:08:56 +0430

    +Example 04   -typedef struct   -volatile

--- User/main.c
+++ User/main.c
@@ -1,13 +1,29 @@
-int main()
+typedef struct
+{
+  volatile unsigned int MODER;
+  volatile unsigned int OTYPER;
+  volatile unsigned int OSPEEDER;
+  volatile unsigned int PUPDR;
+  volatile unsigned int IDR;
+  volatile unsigned int ODR;
+  volatile unsigned int BSRR;
+  volatile unsigned int LCKR;
+  volatile unsigned int AFRL;
+  volatile unsigned int AFRH;
+} GPIO_t;
+#define GPIOC  ((GPIO_t *)0x40020800)
+#define GPIOE  ((GPIO_t *)0x40021000)
+
+int main(void)
 {
   *(unsigned int *)(0x40023800 + 0x30) |= 0x14; // RCC_AHB1ENR = (1 << 4) | (1 << 2)
-  *(unsigned int *)0x40020800 |= 1 << (2 * 13); // GPIOC_MODER
+  GPIOC->MODER |= 1 << (2 * 13); // GPIOC_MODER
   
   while(1)
   {
-    if((*(unsigned int *)(0x40021000 + 0x10) & (1 << 4)) == 0) // GPIOE_IDR
-      *(unsigned int *)(0x40020800 + 0x14) &= ~(unsigned int)(1 << 13); // GPIOC_ODR
+    if((GPIOE->IDR & (1 << 4)) == 0) // GPIOE_IDR
+      GPIOC->ODR &= ~(unsigned int)(1 << 13); // GPIOC_ODR
     else
-      *(unsigned int *)(0x40020800 + 0x14) |= (1 << 13); // GPIOC_ODR
+      GPIOC->ODR |= (1 << 13); // GPIOC_ODR
   }
 }
